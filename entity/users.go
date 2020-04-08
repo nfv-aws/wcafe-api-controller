bcf5615151e65bfb61259adc3f1cf9b17726837f
zo <ecocoa.47@gmail.com>
2020-04-08 14:08:39 +0900

    Modify Users POST&GET (#23)
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-45-1.ap-northeast-1.compute.internal>

---
+++ entity/users.go
@@ -0,0 +1,23 @@
+package entity
+
+type User struct {
+
+	// 商品の識別子
+	Id string `json:"id"`
+
+	// 商品種
+	Number string `json:"number"`
+
+	// 商品名
+	Name string `json:"name"`
+
+	// 商品の年齢
+	Address string `json:"address"`
+
+	// 店の識別子
+	Email string `json:"email"`
+}
+
+type Users struct {
+	Users *[]User
+}
