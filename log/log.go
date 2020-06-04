b18d21becfc35f2cf049803d7d9126036a590bb8
ito-katsu <katsuma.ito.cp@nttcom.co.jp>
2020-06-04 05:54:40 +0000

    add main.log

---
+++ log/log.go
@@ -0,0 +1,70 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nfv-aws/wcafe-api-controller/config"
+)
+
+func ServerLog() *gin.Engine {
+	config.Configure()
+	file_path := config.C.LOG.File_path
+
+	// ログファイルに出力する際の色設定
+	gin.ForceConsoleColor()
+
+	// Logging to a file.
+	f, _ := os.Create(file_path + "gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	// Use the following code if you need to write the logs to file and console at the same time.
+	// gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	router := gin.Default()
+
+	// LoggerWithFormatter middleware will write the logs to gin.DefaultWriter
+	// By default gin.DefaultWriter = os.Stdout
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	router.Use(gin.Recovery())
+
+	return router
+}
+
+func LoggingSettings() {
+	// RDWRはreadとwrite。パーミッションで0666は読み書きができるユーザーその他。
+	logfile, _ := os.OpenFile("./log/gin.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	gin.DefaultWriter = io.MultiWriter(logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(gin.DefaultWriter)
+}
+
+/*
+func Log() {
+	logfile, err := os.OpenFile("./log/gin.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	gin.DefaultWriter = io.MultiWriter(os.Stdout, logfile)
+	//	r := gin.Default()
+	log.SetOutput(gin.DefaultWriter)
+}
+*/
--- server/server.go
+++ server/server.go
@@ -1,15 +1,11 @@
 package server
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"time"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/nfv-aws/wcafe-api-controller/config"
+
 	"github.com/nfv-aws/wcafe-api-controller/controller"
+	logger "github.com/nfv-aws/wcafe-api-controller/log"
 	"github.com/nfv-aws/wcafe-api-controller/service"
 )
 
@@ -19,45 +15,8 @@ func Init() {
 	r.Run()
 }
 
-func logger() *gin.Engine {
-	config.Configure()
-	file_path := config.C.LOG.File_path
-
-	// ログファイルに出力する際の色設定
-	gin.ForceConsoleColor()
-
-	// Logging to a file.
-	f, _ := os.Create(file_path + "gin.log")
-	gin.DefaultWriter = io.MultiWriter(f)
-
-	// Use the following code if you need to write the logs to file and console at the same time.
-	// gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
-
-	router := gin.Default()
-
-	// LoggerWithFormatter middleware will write the logs to gin.DefaultWriter
-	// By default gin.DefaultWriter = os.Stdout
-	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-
-		// your custom format
-		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
-			param.ClientIP,
-			param.TimeStamp.Format(time.RFC1123),
-			param.Method,
-			param.Path,
-			param.Request.Proto,
-			param.StatusCode,
-			param.Latency,
-			param.Request.UserAgent(),
-			param.ErrorMessage,
-		)
-	}))
-	router.Use(gin.Recovery())
-	return router
-}
-
 func router() *gin.Engine {
-	r := logger()
+	r := logger.ServerLog()
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"*"}
 	r.Use(cors.New(config))
