1b0d838fe905e32e7ac90a59f8504e3a68c8846e
ito-katsu <62636292+ito-katsu@users.noreply.github.com>
2020-06-05 16:19:31 +0900

    Fix log print (#81)
    * add main.log
    
    * remove not use func

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/nfv-aws/wcafe-api-controller/db"
+	logger "github.com/nfv-aws/wcafe-api-controller/log"
 	"github.com/nfv-aws/wcafe-api-controller/server"
 )
 
 func main() {
+	logger.LoggingSettings()
 	db.Init()
 	server.Init()
 	db.Close()
