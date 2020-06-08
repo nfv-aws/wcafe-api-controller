479a2fb20770f0d67549db84586879aa9c6392ef
Yasushi YOSHIDA <aeolianharp.op10@gmail.com>
2020-06-08 10:10:49 +0900

    Fix Logger (#82)

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/nfv-aws/wcafe-api-controller/db"
-	logger "github.com/nfv-aws/wcafe-api-controller/log"
 	"github.com/nfv-aws/wcafe-api-controller/server"
 )
 
 func main() {
-	logger.LoggingSettings()
 	db.Init()
 	server.Init()
 	db.Close()
